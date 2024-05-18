bc7d61fbdb14487d2652fda865c2576f95b506cd
Waka-G <140310246+Waka-G@users.noreply.github.com>
2024-05-18 09:41:05 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>kadai_007 連想配列を作って値とキーを出力しよう</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      //連想配列を作成する
+      $array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      //値を出力する
+      print_r($array);
+      ?>
+    </p>
+  </body>
+</html>
